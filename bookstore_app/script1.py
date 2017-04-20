9f06c6fe40c322b219e294b771c73cae69211a78
Henry.Morrin <hmorrin@hotmail.com>
2017-04-20 18:28:14 +0100

    Bookstore frontendwidgets

--- bookstore_app/script1.py
+++ bookstore_app/script1.py
@@ -1,1 +1,39 @@
 #This will be the python flask script -> to create the front end of the bookstore app
+
+#Import the Twkinter library - to create the frontend application
+
+from Tkinter import *
+
+
+window=Tk()
+
+l1=Label(window,text="Title")
+l1.grid(row=0, column=0)
+
+l2=Label(window,text="Author")
+l2.grid(row=0, column=2)
+
+l1=Label(window,text="Name")
+l1.grid(row=1, column=0)
+
+l1=Label(window,text="ISBN")
+l1.grid(row=1, column=2)
+#Now create texts boxes
+
+title_var=StringVar()
+E1=Entry(window,textvariable=title_var)
+E1.grid(row=0, column=1)
+
+Author_var=StringVar()
+E2=Entry(window,textvariable=Author_var)
+E2.grid(row=0, column=3)
+
+Name_var=StringVar()
+E3=Entry(window,textvariable=Name_var)
+E3.grid(row=1, column=1)
+
+ISBN_var=StringVar()
+E4=Entry(window,textvariable=ISBN_var)
+E4.grid(row=1, column=3)
+
+window.mainloop()
